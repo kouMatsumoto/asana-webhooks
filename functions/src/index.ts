450ba67955f42cd36e26ca187c491b69b101b66d
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-08-14 21:17:29 +0900

    feat: add a function for webhooks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,19 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const webhooksTest = functions
+  .region('asia-northeast1')
+  .timeZone('Asia/Tokyo')
+  .https.onRequest(async (request, response) => {
+    functions.logger.info('called');
+
+    if (request.method !== 'POST') {
+      functions.logger.error('post request required');
+      response.status(400).send('error');
+
+      return;
+    }
+
+    functions.logger.info('body', request.body);
+    response.status(200).send();
+  });
